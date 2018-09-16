5ed2ac9ae07ba0e0547b39fb62870ac0959faaa4
Mohamad Radisha <pr0ph0z23@gmail.com>
2018-09-16 22:11:00 +0700

    Fix container height

--- resources/views/include/body.blade.php
+++ resources/views/include/body.blade.php
@@ -21,7 +21,7 @@
           <div class="row">
 
             <div class="col-md-12 col-sm-12 col-xs-12">
-              <div class="x_panel" style="height:600px;">
+              <div class="x_panel">
                 <div class="x_title">
                   <div class="clearfix"></div>
                 </div>
